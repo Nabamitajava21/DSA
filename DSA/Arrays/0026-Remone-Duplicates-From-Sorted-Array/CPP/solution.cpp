d87cfe38c8d06f9aba48bd0ab3793147c2521829
Nabamita Nath <168272093+Nabamitajava21@users.noreply.github.com>
2026-08-05 23:49:02 +0530

    Update solution.cpp

--- DSA/Arrays/0026-Remone-Duplicates-From-Sorted-Array/CPP/solution.cpp
+++ DSA/Arrays/0026-Remone-Duplicates-From-Sorted-Array/CPP/solution.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        //Nabamita
+        int i = 1;
+
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[j - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+};
